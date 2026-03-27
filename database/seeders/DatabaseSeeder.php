7b304eeacc7b65de63789dff285bcad6970649b7
Zaheer Irshad <zaheer.python@enzipe.net>
2026-03-27 11:09:48 +0500

    inia

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -1,25 +1,11 @@
-<?php
-
-namespace Database\Seeders;
-
 use App\Models\User;
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
-use Illuminate\Database\Seeder;
 
-class DatabaseSeeder extends Seeder
-{
-    use WithoutModelEvents;
-
-    /**
-     * Seed the application's database.
-     */
-    public function run(): void
-    {
-        // User::factory(10)->create();
-
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
-    }
-}
+User::updateOrCreate(
+    ['email' => 'test@example.com'],
+    [
+        'name' => 'Test User',
+        'password' => bcrypt('password123'),
+        'email_verified_at' => now(),
+        'remember_token' => Str::random(10),
+    ]
+);
